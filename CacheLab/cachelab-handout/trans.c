1a633f07e110803963e08169fe9c2e8b012f8981
B4night <2529633213@qq.com>
2023-01-05 15:46:09 +0800

    remove trivial files

--- CacheLab/cachelab-handout/trans.c
+++ CacheLab/cachelab-handout/trans.c
@@ -12,6 +12,81 @@
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
+void process_32(int A[32][32], int B[32][32]) {
+    int i, j, x, y;
+    int t1, t2, t3, t4, t5, t6, t7, t8;
+    //(i, j)为每个块左上角的元素的坐标
+    for (i = 0; i < 32; i += 8) {
+        for (j = 0; j < 32; j += 8) {
+            if (i != j) {	//处理非对角线上的块
+                for (x = i; x < i + 8; x++) {
+                    for (y = j; y < j + 8; y++) {
+                        B[y][x] = A[x][y];
+                    }
+                }
+            } else {
+                //处理对角线上的块
+                x = i;	//一次性复制A[i][j]这一块到B[i][j]这一块上
+                t1 = A[x][j]; t2 = A[x][j+1]; t3 = A[x][j+2]; t4 = A[x][j+3]; 
+                t5 = A[x][j+4]; t6 = A[x][j+5]; t7 = A[x][j+6]; t8 = A[x][j+7];
+                B[x][j] = t1; B[x][j+1] = t2; B[x][j+2] = t3; B[x][j+3] = t4; 
+                B[x][j+4] = t5; B[x][j+5] = t6; B[x][j+6] = t7; B[x][j+7] = t8;
+
+                x++; 	//一次性复制A[i+1][j]这一块到B[i+1][j]这一块上
+                t1 = A[x][j]; t2 = A[x][j+1]; t3 = A[x][j+2]; t4 = A[x][j+3]; 
+                t5 = A[x][j+4]; t6 = A[x][j+5]; t7 = A[x][j+6]; t8 = A[x][j+7];
+                B[x][j] = t1; B[x][j+1] = t2; B[x][j+2] = t3; B[x][j+3] = t4; 
+                B[x][j+4] = t5; B[x][j+5] = t6; B[x][j+6] = t7; B[x][j+7] = t8;
+
+                x++; 	//...
+                t1 = A[x][j]; t2 = A[x][j+1]; t3 = A[x][j+2]; t4 = A[x][j+3]; 
+                t5 = A[x][j+4]; t6 = A[x][j+5]; t7 = A[x][j+6]; t8 = A[x][j+7];
+                B[x][j] = t1; B[x][j+1] = t2; B[x][j+2] = t3; B[x][j+3] = t4; 
+                B[x][j+4] = t5; B[x][j+5] = t6; B[x][j+6] = t7; B[x][j+7] = t8;
+
+                x++; 
+                t1 = A[x][j]; t2 = A[x][j+1]; t3 = A[x][j+2]; t4 = A[x][j+3]; 
+                t5 = A[x][j+4]; t6 = A[x][j+5]; t7 = A[x][j+6]; t8 = A[x][j+7];
+                B[x][j] = t1; B[x][j+1] = t2; B[x][j+2] = t3; B[x][j+3] = t4; 
+                B[x][j+4] = t5; B[x][j+5] = t6; B[x][j+6] = t7; B[x][j+7] = t8;
+
+                x++; 
+                t1 = A[x][j]; t2 = A[x][j+1]; t3 = A[x][j+2]; t4 = A[x][j+3]; 
+                t5 = A[x][j+4]; t6 = A[x][j+5]; t7 = A[x][j+6]; t8 = A[x][j+7];
+                B[x][j] = t1; B[x][j+1] = t2; B[x][j+2] = t3; B[x][j+3] = t4; 
+                B[x][j+4] = t5; B[x][j+5] = t6; B[x][j+6] = t7; B[x][j+7] = t8;
+
+                x++; 
+                t1 = A[x][j]; t2 = A[x][j+1]; t3 = A[x][j+2]; t4 = A[x][j+3]; 
+                t5 = A[x][j+4]; t6 = A[x][j+5]; t7 = A[x][j+6]; t8 = A[x][j+7];
+                B[x][j] = t1; B[x][j+1] = t2; B[x][j+2] = t3; B[x][j+3] = t4;
+                B[x][j+4] = t5; B[x][j+5] = t6; B[x][j+6] = t7; B[x][j+7] = t8;
+
+                x++; 
+                t1 = A[x][j]; t2 = A[x][j+1]; t3 = A[x][j+2]; t4 = A[x][j+3]; 
+                t5 = A[x][j+4]; t6 = A[x][j+5]; t7 = A[x][j+6]; t8 = A[x][j+7];
+                B[x][j] = t1; B[x][j+1] = t2; B[x][j+2] = t3; B[x][j+3] = t4; 
+                B[x][j+4] = t5; B[x][j+5] = t6; B[x][j+6] = t7; B[x][j+7] = t8;
+
+                x++; 	////一次性复制A[i+7][j]这一块到B[i+7][j]这一块上
+                t1 = A[x][j]; t2 = A[x][j+1]; t3 = A[x][j+2]; t4 = A[x][j+3]; 
+                t5 = A[x][j+4]; t6 = A[x][j+5]; t7 = A[x][j+6]; t8 = A[x][j+7];
+                B[x][j] = t1; B[x][j+1] = t2; B[x][j+2] = t3; B[x][j+3] = t4; 
+                B[x][j+4] = t5; B[x][j+5] = t6; B[x][j+6] = t7; B[x][j+7] = t8;
+	
+                //此时,B中的黄色块上的元素和A中的完全相同,现在需要将这一部分转置
+                for (x = i; x < i + 8; x++) {
+                    for (y = j; y < j + (x - i); y++) {
+                        t1 = B[x][y];
+                        B[x][y] = B[y][x];
+                        B[y][x] = t1;
+                    }
+                }
+            }
+        }
+    }
+}
+
 /* 
  * transpose_submit - This is the solution transpose function that you
  *     will be graded on for Part B of the assignment. Do not change
@@ -22,6 +97,9 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    if (M == 32) {
+        process_32(A, B);
+    }
 }
 
 /* 
